ef4315acf6102b4f88566063d5ce708ad9a0b9d1
envis10n <envis10n@envis10n.dev>
2022-03-11 15:12:08 -0600

    Update axiod to 0.24

--- deps/axiod.ts
+++ deps/axiod.ts
@@ -1,3 +1,3 @@
-import * as _axiod from "https://raw.githubusercontent.com/envis10n/axiod/master/mod.ts";
+import * as _axiod from "https://deno.land/x/axiod@0.24/mod.ts";
 
 export const axiod = _axiod.default;

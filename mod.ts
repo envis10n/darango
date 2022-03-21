3c965993935f757ae1ce8a6442e0e99879a6acd6
envis10n <envis10n@envis10n.dev>
2022-03-21 14:16:24 -0500

    Add Edge and EdgeCollection to mod.ts

--- mod.ts
+++ mod.ts
@@ -3,3 +3,4 @@ export { ArangoCursor } from "./cursor.ts";
 export type { Cursor, CursorOptions } from "./cursor.ts";
 export { Collection } from "./collection.ts";
 export type { Document, DocumentBase, DocumentData } from "./collection.ts";
+export { Edge, EdgeCollection } from "./edge.ts";
